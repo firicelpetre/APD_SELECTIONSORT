d444ffc41bbf05692a1c04adbbdc80d3b80afa94
firicelpetre <100560972+firicelpetre@users.noreply.github.com>
2023-04-04 19:28:01 +0300

    commit

--- sortareprinselectie/Utils.h
+++ sortareprinselectie/Utils.h
@@ -4,15 +4,7 @@
 
 using namespace std;
 
-/*
-* se vor genera 6 teste astfel:
-* - primul fisier de test va contine 10 elemente, nr intregi, cuprinse intre 1 si 100 --fout << rand() % 100 + 1 << " ";
-* - urmatorul fisier va contine 50 de elemente cuprinse intre 1 si 1000 --fout << rand() % 1000 + 1 << " ";
-* - cel de-al treilea fisier va contine 100 de elemente cuprinse intre 1 si 10000 --fout << rand() % 10000 + 1 << " ";
-* - al patrulea fisier va contine 150 elemente cuprinse intre 1 si 100000 --fout << rand() % 100000 + 1 << " ";
-* - al cincilea fisier contine 200 de elemente cuprinse intre 1 si 1000000 --fout << rand() % 1000000 + 1 << " ";
-* - iar, al saselea fisier va contine 10000 de elemente cuprinse intre 1 si 1000000 --fout << rand() % 1000000 + 1 << " ";
-*/
+
 
 void test_generator();
 
